6de9e014eb6040ecee9464d0a9bac56f4b9c1270
Berhan <57452604+berhanserin@users.noreply.github.com>
2022-01-11 18:45:15 +0300

    yayin

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
 const express = require('express');
 const socketIO = require('socket.io');
-
-const PORT = process.env.PORT || 3000;
+const axios=require("axios");
+const PORT = process.env.PORT || 9000;
 const INDEX = '/index.html';
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server, { cors: { orgins: '*' } });
 
 io.on('connection', (socket) => {
   console.log('Client connected');
-  // const data = axios
-  //   .get('https://api.ajansspor.com/apiv2/matches/soccer/getLiveMacthes', { params: { date: '11-01-2022' } })
-  //   .then((result) => {
-  //     console.log(result.data.liveMatches);
-  //     socket.emit('real', result.data.liveMatches);
-  //   })
-  //   .catch((error) => error.message);
+  const data = axios
+    .get('https://api.ajansspor.com/apiv2/matches/soccer/getLiveMacthes', { params: { date: '11-01-2022' } })
+    .then((result) => {
+      socket.emit('real', result.data.liveMatches);
+    })
+    .catch((error) => error.message);
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
