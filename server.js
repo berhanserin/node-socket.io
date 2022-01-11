9475e640ec2ee905d71164ee6cba7cfa5683e42d
Berhan <57452604+berhanserin@users.noreply.github.com>
2022-01-11 18:49:12 +0300

    moment add

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const socketIO = require('socket.io');
 const axios=require("axios");
 const PORT = process.env.PORT || 9000;
 const INDEX = '/index.html';
-
+const moment=require("moment");
 const server = express()
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
@@ -12,7 +12,7 @@ const io = socketIO(server, { cors: { orgins: '*' } });
 io.on('connection', (socket) => {
   console.log('Client connected');
   const data = axios
-    .get('https://api.ajansspor.com/apiv2/matches/soccer/getLiveMacthes', { params: { date: '11-01-2022' } })
+    .get('https://api.ajansspor.com/apiv2/matches/soccer/getLiveMacthes', { params: { date: moment(new Date()).format("DD-MM-YYYY") } })
     .then((result) => {
       socket.emit('real', result.data.liveMatches);
     })
