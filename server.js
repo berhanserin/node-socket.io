ff5978d6d855709d2c09a7b370b627c1e9613f1b
Berhan <57452604+berhanserin@users.noreply.github.com>
2022-01-11 18:30:11 +0300

    init2

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const io = socketIO(server);
+  const io = socketIO(server, { cors: { orgins: '*' } });
 
 io.on('connection', (socket) => {
   console.log('Client connected');
